55945415b11cea2a984698f9ec16addbc07b5ae7
Noam97 <noamm240@gmail.com>
2024-06-07 15:52:38 +0300

    create a table and select for the  closeness

---
+++ ui/src/components/Card.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="flex justify-around gap-16 border border-blue-200 rounded-xl shadow-md px-6 py-2 w-full">
+    <div class="flex items-center gap-3 h-[80px]">
+      <img src="../assets/images/brideAndGroom.png" alt="wedding brideAndGroom" class="w-16 h-16" />
+      <div class="flex flex-col">
+        <span class="text-3xl">
+          250
+        </span>
+        <span class="text-xs">
+          guests
+        </span>
+      </div>
+      <div class="flex flex-col justify-evenly h-full">
+        <span class="text-xs">
+          130 brides
+        </span>
+        <span class="text-xs">
+          120 grooms
+        </span>
+      </div>
+    </div>
+
+    <div class="flex items-center gap-3 h-[80px]">
+      <img src="../assets/images/letter.png" alt="wedding brideAndGroom" class="w-16 h-16" />
+      <div class="flex flex-col">
+        <span class="text-3xl">
+          25
+        </span>
+        <span class="text-xs">
+          invitations
+        </span>
+      </div>
+    </div>
+
+    <div class="flex items-center gap-3 h-[80px]">
+      <div class="flex flex-col">
+        <span class="text-lg">
+          Export from excel
+        </span>
+        <span class="text-lg">
+          Import to excel
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
