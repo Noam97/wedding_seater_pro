e68d50f1c7411ccf9248b565376fbf0b9441c877
Noam97 <noamm240@gmail.com>
2024-06-19 22:16:49 +0300

    home page

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
-  <guest-management />
+  <div>
+    <Navigation />
+    <guest-management />
+  </div>
 </template>
 
 <script setup>
 import GuestManagement from "@/components/GuestManagement.vue";
+import Navigation from "@/components/Navigation.vue";
 </script>
