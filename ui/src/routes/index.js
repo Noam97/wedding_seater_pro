05df1fad7ad5bf165a1c0a8eaac9315f6691c16b
Noam97 <noamm240@gmail.com>
2024-07-02 19:43:04 +0300

    check if logged in else redirect to login page

--- ui/src/routes/index.js
+++ ui/src/routes/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import {useStore} from "@/store/index.js";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,20 @@ const router = createRouter({
             component: () => import('../views/Login.vue')
         }
     ]
+})
+
+router.beforeEach(async (to, from, next) => {
+    const pageAuthRequired = !(['/login', '/register'].includes(to.path))
+    const token = useStore().getToken
+
+    if (pageAuthRequired && !token) {
+        console.debug(
+            'auth required and no user object... redirecting to login'
+        )
+        return next({ name: 'login' })
+    }
+
+    next()
 })
 
 export default router
