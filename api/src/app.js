12e1eedd35dcd3c62568c699f37d21dc47160cf8
Noam97 <noamm240@gmail.com>
2024-07-05 12:07:42 +0300

    algorithm

--- api/src/app.js
+++ api/src/app.js
@@ -76,8 +76,7 @@ app.post('/api/guests', authenticateToken, async (req, res) => {
             return res.status(400).send({ error: 'The payload is missing' })
 
         // Create a new guest into the database
-        const newGuest = await prisma.guest.create({
-            data: {
+        const newTable = await prisma.table.create({            data: {
                 name,
                 count: guestsCount,
                 side,
@@ -86,7 +85,7 @@ app.post('/api/guests', authenticateToken, async (req, res) => {
             },
         });
 
-        res.status(201).json(newGuest);
+        res.status(201).json(newTable);
     } catch (error) {
         console.error('Error creating a guest:', error);
         res.status(500).json({ error: 'Internal server error' });
@@ -103,7 +102,7 @@ app.get('/api/guests', authenticateToken, async (req, res) => {
                 table: true,
             },
         });
-        res.status(200).json(guests);
+        res.status(200).json(tables);
     } catch (error) {
         res.status(500).json({ error: 'Internal server error' });
     }
@@ -154,17 +153,59 @@ app.post('/api/tables/generate', authenticateToken, async (req, res) => {
     }
 })
 
-app.get('/api/tables/genereta', async (req, res) => {
-    // @TODO the real algorithm is here !!!!
+app.get('/api/tables/generate', authenticateToken, async (req, res) => {
+    try {
+        const guestsByUser = await prisma.guest.findMany({
+            select: {
+                side: true,
+                closeness: true,
+                count: true,
+                name: true,
+                id: true,
+            },
+            where: {
+                user_id: req.user.id
+            },
+        });
+        const groupedGuests = guestsByUser.reduce((acc, guest) => {
+            const key = `${guest.side}-${guest.closeness}`;
+            acc[key] = acc[key] || {side: guest.side, closeness: guest.closeness, guestsCount: 0, guests: []};
+            acc[key].guestsCount += guest.count;
+            acc[key].guests.push(guest);
+            return acc;
+        }, {});
 
-    // try {
-    //     const guests = await prisma.guest.findMany();
-    //     res.status(200).json(guests);
-    // } catch (error) {
-    //     res.status(500).json({ error: 'Internal server error' });
-    // }
+        const sortedGuests = Object.values(groupedGuests).sort((a, b) => a.guestsCount - b.guestsCount);
+
+        const tables = await prisma.table.findMany({
+            where: {
+                user_id: req.user.id
+            },
+            orderBy: {
+                places_count: 'asc'
+            }
+        });
+        const list = []
+        let [tableIndex, guestIndex] = [0, 0]
+
+        while (tableIndex < tables.length) {
+            if (tables[tableIndex]['places_count'] >= sortedGuests[guestIndex].guestsCount) {
+                list.push({
+                    table: tables[tableIndex],
+                    guests: sortedGuests[guestIndex].guests,
+                    guestsCount: sortedGuests[guestIndex].guestsCount
+                })
+                guestIndex++
+            }
+
+            tableIndex++
+        }
+
+        res.status(200).json(list);
+    } catch (error) {
+        res.status(500).json({error: 'Internal server error'});
+     }
 })
-
 app.listen(PORT, () => {
     console.log(`Server listening at port: ${PORT}`);
 });
